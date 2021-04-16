a77fb0895dbdf4ab60227a80a20cdd6d03200abb
Aporve <aporvep@gmail.com>
2021-04-16 14:43:55 +0530

    Updated code

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-document.getElementById('ym-notification').style.bottom = '70px';
-var elems = document.getElementsByClassName("from-icon");
-for (var i = 0; i < elems.length; i++) {
-    elems[i].style.backgroundSize = "39px";
-}
-
+document.getElementById('ym-notification').style.bottom = '80px';
 var observer = new MutationObserver(function(mutations) {
-	document.getElementById('ym-notification').style.bottom = '70px';
+	document.getElementById('ym-notification').style.bottom = '80px';
 });
 var target = document.getElementById('ym-notification');
 observer.observe(target, { attributes : true, attributeFilter : ['style'] });
-
