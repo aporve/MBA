8b5d8cf441ae8a00958c31d08a95ce1963118468
Aporve <aporvep@gmail.com>
2021-04-16 14:33:16 +0530

    Changed COde

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById('ym-notification').style.bottom = '70px';
 var observer = new MutationObserver(function(mutations) {
 	document.getElementById('ym-notification').style.bottom = '70px';
-	document.getElementById("from-icon").style.backgroundSize = "39px";
+	document.getElementsByClassName("from-icon").style.backgroundSize = "39px";
 });
 var target = document.getElementById('ym-notification');
 observer.observe(target, { attributes : true, attributeFilter : ['style'] });
