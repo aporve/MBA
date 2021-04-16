de3cc44530e3d91d0ec7c29ffc11f1134264c5b6
Aporve <aporvep@gmail.com>
2021-04-16 14:35:06 +0530

    Change background size

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById('ym-notification').style.bottom = '70px';
+document.getElementsByClassName("from-icon").style.backgroundSize = "39px";
 var observer = new MutationObserver(function(mutations) {
 	document.getElementById('ym-notification').style.bottom = '70px';
-	document.getElementsByClassName("from-icon").style.backgroundSize = "39px";
 });
 var target = document.getElementById('ym-notification');
 observer.observe(target, { attributes : true, attributeFilter : ['style'] });
