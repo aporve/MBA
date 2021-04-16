d33204b96b2f6bfee3bd5b9b6a3cc2a456d2d457
Aporve <aporvep@gmail.com>
2021-04-16 14:25:09 +0530

    Updated code

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 document.getElementById('ym-notification').style.bottom = '70px';
 var observer = new MutationObserver(function(mutations) {
 	document.getElementById('ym-notification').style.bottom = '70px';
+	document.getElementById("from-icon").style.backgroundSize = "39px";
 });
 var target = document.getElementById('ym-notification');
 observer.observe(target, { attributes : true, attributeFilter : ['style'] });
 
-document.getElementById("from-icon").style.backgroundSize = "39px";
